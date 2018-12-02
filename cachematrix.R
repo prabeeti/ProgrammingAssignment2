8758927f445c186778b45cd3e03de074a6c878eb
Prabeeti <prabetei@gmail.com>
2018-12-02 22:39:27 +0800

    comments modifie

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 
 
 #Assignment 2 for Lexical Scoping
+## makeCacheMatrix is a function which creates a special "matrix" object that can 
+## cache its inverse for the input (which is an invertible square matrix)
 makeCacheMatrix <- function(x = matrix()) {
   check_inverse<-NULL
   set<-function(y){
@@ -14,6 +16,10 @@ makeCacheMatrix <- function(x = matrix()) {
        set_inverse=set_inverse,
        get_inverse=get_inverse)
 }
+
+## cacheSolve is a function which computes the inverse of the special "matrix" 
+## returned by makeCacheMatrix above. 
+
 
 cacheSolve <- function(x=matrix(), ...) {
   check_inverse<-x$get_inverse()
